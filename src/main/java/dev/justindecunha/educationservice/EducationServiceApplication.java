62fc6c1dd2d617b43b32d57577451245f05fa70d
JDeCunha <justin.decunha.bath@gmail.com>
2024-05-23 09:13:00 -0400

    Bootstrap service with Spring Initializr
    - https://start.spring.io

---
+++ src/main/java/dev/justindecunha/educationservice/EducationServiceApplication.java
@@ -0,0 +1,13 @@
+package dev.justindecunha.educationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EducationServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EducationServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/justindecunha/educationservice/EducationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.justindecunha.educationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EducationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
